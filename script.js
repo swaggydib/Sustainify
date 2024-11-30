57f470c37a679cf340c89f38fd848228dded66b2
swaggydib <23523276@students.uii.ac.id>
2024-11-30 10:12:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function togglemenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  loop: true,
+  centerSlides: "true",
+  grabCursor: "true",
+  fade: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
