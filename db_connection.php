2c4f52fae579243394a44fdc8ae872412cf5ca2f
swaggydib <23523276@students.uii.ac.id>
2024-12-19 18:36:40 +0700

    back to awal punya adib

--- db_connection.php
+++ db_connection.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'sustainify';
+$database = 'sdg_project';
 
 // Create connection
 $conn = new mysqli($host, $username, $password, $database);
